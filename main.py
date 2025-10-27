6aa816368483b939a7877aff111c32a1ff7daf51
Archana kanchimireddy <72028908+archana-2813@users.noreply.github.com>
2025-10-27 03:27:29 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Time Series Problem – Additive Model Analysis
+Developed as part of Data Analytics coursework.
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+# Example structure
+print("Time Series Forecasting Project")
